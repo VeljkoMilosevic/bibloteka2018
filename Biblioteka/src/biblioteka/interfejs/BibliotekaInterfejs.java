964a548c2ccbe1c0fdf133e20406bc0233203dfe
VeljkoMilosevic <veljkoveljko@hotmail.com>
2018-03-02 11:36:09 +0100

    Uvedan interfejs BilotekaInterfejs
    Premestane klasa Autor i Knjiga

---
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -0,0 +1,11 @@
+package biblioteka.interfejs;
+
+import biblioteka.Knjiga;
+
+public interface BibliotekaInterfejs {
+
+	public void dodajKnjigu(Knjiga k);
+	public void obrisiKnjigu (Knjiga k);
+	
+	
+}
